693acac56aa6a13191b01587fe327cd51d17d88d
tallyto <rodrigues.tallyto@hotmail.com>
2023-05-29 23:17:47 -0300

    11.3 vinculando propriedades do hospedeiro com hostbiding

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,9 +1,10 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core'
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core'
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
+
   constructor (
     private readonly elementRef: ElementRef,
     private readonly renderer: Renderer2
@@ -11,13 +12,17 @@ export class CampoColoridoDirective {
 
   }
 
+  @HostBinding('style.backgroundColor')
+  corDeFundo: string | undefined
+
   @HostListener('focus')
   onFocus () {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow')
+    this.corDeFundo = 'green'
   }
 
   @HostListener('blur')
   onBlur () {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white')
+    this.corDeFundo = 'transparent'
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
   }
 }
