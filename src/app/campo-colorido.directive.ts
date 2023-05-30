b0f6be72eeafcc849d29dcd1e947dbffe794ba8a
tallyto <rodrigues.tallyto@hotmail.com>
2023-05-29 23:10:19 -0300

    11.2 - respondendo a eventos do hospedeiro com host listener

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core'
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core'
 
 @Directive({
   selector: '[appCampoColorido]'
@@ -8,6 +8,16 @@ export class CampoColoridoDirective {
     private readonly elementRef: ElementRef,
     private readonly renderer: Renderer2
   ) {
+
+  }
+
+  @HostListener('focus')
+  onFocus() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow')
+  }
+
+  @HostListener('blur')
+  onBlur() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white')
   }
 }
