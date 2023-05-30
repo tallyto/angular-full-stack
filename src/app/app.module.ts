98caf3c59b52c6c6104a5876e3387c94b2f9065e
tallyto <rodrigues.tallyto@hotmail.com>
2023-05-30 12:54:55 -0300

    11.6 - conhecendo e usando pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { FormsModule } from '@angular/forms'
 import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component'
 import { CampoColoridoDirective } from './campo-colorido.directive'
-import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component'
+import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+import { PessoaComponent } from './pessoa/pessoa.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FuncionarioCardComponent,
     CampoColoridoDirective,
-    FuncionarioFormComponent
+    FuncionarioFormComponent,
+    PessoaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pessoa/pessoa.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa',
+  templateUrl: './pessoa.component.html',
+  styleUrls: ['./pessoa.component.css']
+})
+export class PessoaComponent {
+  nome = 'Tállyto Rodrigues'
+  dataAniversario = new Date(1996,4, 6);
+  preco = 12855.32;
+  troco = 0.57392;
+
+}
