3ad8265a9f70078f4f1111bbbf69169101ee18c3
tallyto <rodrigues.tallyto@hotmail.com>
2023-06-05 11:24:21 -0300

    11.7 passando parametros para pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
@@ -9,7 +9,9 @@ import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.co
 import { CampoColoridoDirective } from './campo-colorido.directive'
 import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
 import { PessoaComponent } from './pessoa/pessoa.component'
-
+import {registerLocaleData} from "@angular/common";
+import ptBr from '@angular/common/locales/pt'
+registerLocaleData(ptBr)
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +26,9 @@ import { PessoaComponent } from './pessoa/pessoa.component'
     NgbModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
