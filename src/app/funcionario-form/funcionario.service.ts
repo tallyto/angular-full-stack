5a563c7099defaba6d287d5922df621b0da8cfc2
tallyto <rodrigues.tallyto@hotmail.com>
2023-07-09 11:41:13 -0300

    15.7 - injetando servicos dentro de services e o decorator injectable

--- src/app/funcionario-form/funcionario.service.ts
+++ src/app/funcionario-form/funcionario.service.ts
@@ -1,7 +1,14 @@
+import {LogService} from "../log.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
 export class FuncionarioService {
   funcionarios: Funcionario[] = [{id: 1, nome: 'João'}]
 
+  constructor(private logService: LogService) {
+  }
   public adicionar(nome: string) : void {
+    this.logService.log(`Adicionando nome ${nome}...`)
     const funcionario: Funcionario = {id: this.funcionarios.length + 1, nome}
     this.funcionarios.push(funcionario)
     console.log(this.funcionarios)
